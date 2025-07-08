bd829c7d6ee208532115de255c644bbb964077e4
JoJo <salomj9@gmail.com>
2025-07-07 17:39:18 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# Docker Repo
+https://hub.docker.com/r/pacificengine/satisfactory
+
+# Usage
+# Configuration Parameters
+```shell
+serverport=7777
+reliableport=8888
+directory=/home/palworld
+username=palworld
+service=palworld
+version=release
+```
+
+# Setup Commands
+```shell
+mkdir -p "${directory}/logs"
+mkdir -p "${directory}/config"
+mkdir -p "${directory}/saves"
+touch "${directory}/GUID.ini"
+chown $(id -u ${username}):$(id -g ${username}) -R "${directory}"
+chmod 755 -R "${directory}"
+```
+
+# Docker Run Command
+```shell
+docker run -d --name ${service} \
+  --publish ${serverport}:${serverport}/udp \
+  --publish ${serverport}:${serverport}/tcp \
+  --publish ${reliableport}:${reliableport}/tcp \
+  --env PORT_SERVER=${serverport} \
+  --env PORT_RELIABLE=${reliableport} \
+  --env AUTO_UPDATE=true \
+  --env PUID=$(id -u ${username}) \
+  --env PGID=$(id -g ${username}) \
+  --mount type=bind,source=${directory}/logs,target=/home/satisfactory/FactoryGame/Saved/Logs \
+  --mount type=bind,source=${directory}/config,target=/home/satisfactory/FactoryGame/Saved/Config/LinuxServer \
+  --mount type=bind,source=${directory}/saves,target=/home/satisfactory/.config/Epic/FactoryGame/Saved/SaveGames \
+  --mount type=bind,source=${directory}/GUID.ini,target=/home/satisfactory/.config/Epic/FactoryGame/GUID.ini \
+  --restart unless-stopped pacificengine/satisfactory:${version}
+```
+
+# Build
+
+## Clean Environment
+```shell
+docker rm $(docker ps -a -q)
+docker rmi $(docker images -a -q)
+docker volume prune
+docker system prune -a
+```
+
+## Stable
+```shell
+DISTRIBUTION=ubuntu-20
+VERSION=1.1.1.1
+docker build --file "build.Dockerfile" --tag "satisfactory:latest" --build-arg DISTRIBUTION=${DISTRIBUTION} .
+docker image tag satisfactory:latest pacificengine/satisfactory:${DISTRIBUTION}-stable
+docker image tag satisfactory:latest pacificengine/satisfactory:stable
+docker image tag satisfactory:latest pacificengine/satisfactory:${DISTRIBUTION}-latest
+docker image tag satisfactory:latest pacificengine/satisfactory:latest
+docker image tag satisfactory:latest pacificengine/satisfactory:$(git rev-parse --short HEAD)-stable
+docker image tag satisfactory:latest pacificengine/satisfactory:$(git rev-parse --short HEAD)
+docker image tag satisfactory:latest pacificengine/satisfactory:${VERSION}-stable
+docker image tag satisfactory:latest pacificengine/satisfactory:${VERSION}
+docker push pacificengine/satisfactory:${DISTRIBUTION}-stable
+docker push pacificengine/satisfactory:stable
+docker push pacificengine/satisfactory:${DISTRIBUTION}-latest
+docker push pacificengine/satisfactory:latest
+docker push pacificengine/satisfactory:$(git rev-parse --short HEAD)-stable
+docker push pacificengine/satisfactory:$(git rev-parse --short HEAD)
+docker push pacificengine/satisfactory:${VERSION}-stable
+docker push pacificengine/satisfactory:${VERSION}
+```
---
+++ docker/build/server.sh
@@ -0,0 +1,174 @@
+#!/bin/bash
+source /server/regex.sh
+source /server/process.sh
+source /server/properties.sh
+
+INSTALL_DIRECTORY="$(getProperty "INSTALL_DIRECTORY")"
+LOG_DIRECTORY="$(getProperty "LOG_DIRECTORY")"
+USERNAME="$(getProperty "USERNAME")"
+USERGROUP="$(getProperty "USERGROUP")"
+GAME_ID="$(getProperty "GAME_ID")"
+
+IP_SERVER="${IP_SERVER:-$(getProperty "IP_SERVER")}"
+PORT_SERVER="${PORT_SERVER:-$(getProperty "PORT_SERVER")}"
+PORT_RELIABLE="${PORT_RELIABLE:-$(getProperty "PORT_RELIABLE")}"
+AUTO_UPDATE="${AUTO_UPDATE:-$(getProperty "AUTO_UPDATE")}"
+
+DATE="$(date "+%F-%H:%M:%S")"
+LOG_DATE_FORMAT="+%FT%H:%M:%S"
+INPUT_FILE="${LOG_DIRECTORY}/input.log"
+UPDATE_LOG_FILE="${LOG_DIRECTORY}/update.log"
+SIMPLE_LOG_FILE="${LOG_DIRECTORY}/simple.log"
+CURRENT_USERS_FILE="${LOG_DIRECTORY}/user.csv"
+MAIN_LOG_FILE="${LOG_DIRECTORY}/FactoryGame.log"
+PROCESS_ID_FILE="${INSTALL_DIRECTORY}/process.id"
+PROCESS_STATUS_FILE="${INSTALL_DIRECTORY}/process.status"
+UPDATE_SCRIPT="${INSTALL_DIRECTORY}/update.script"
+START_SCRIPT="${INSTALL_DIRECTORY}/FactoryServer.sh"
+START_ARGUMENTS="-multihome=${IP_SERVER} -Port=${PORT_SERVER} -ReliablePort=${PORT_RELIABLE} -log -unattended"
+
+runCommandAsLocalUser() {
+  su --login "${USERNAME}" --shell /bin/bash --command "${@}"
+}
+
+log() {
+  runCommandAsLocalUser "echo '[$(date "${LOG_DATE_FORMAT}")] ${1}' >> '${SIMPLE_LOG_FILE}'"
+}
+
+getServerProcessId() {
+  local id="$(cat "${PROCESS_ID_FILE}")"
+  if [[ -z "${id}" || -z "$(ps --pid ${id} --no-headers)" ]]; then
+    id=$(getProcess '' '\sFactoryGame\s')
+    runCommandAsLocalUser "echo '${id}' > '${PROCESS_ID_FILE}'"
+  fi
+  echo "${id}"
+}
+
+saveLogFiles() {
+  if [[ -f "${INPUT_FILE}" ]]; then
+    runCommandAsLocalUser "mv '${INPUT_FILE}' '${LOG_DIRECTORY}/$(head --lines=1 "${INPUT_FILE}")'"
+  fi
+  if [[ -f "${UPDATE_LOG_FILE}" ]]; then
+    runCommandAsLocalUser "mv '${UPDATE_LOG_FILE}' '${LOG_DIRECTORY}/$(head --lines=1 "${UPDATE_LOG_FILE}")'"
+  fi
+  if [[ -f "${SIMPLE_LOG_FILE}" ]]; then
+    runCommandAsLocalUser "mv '${SIMPLE_LOG_FILE}' '${LOG_DIRECTORY}/$(head --lines=1 "${SIMPLE_LOG_FILE}")'"
+  fi
+  runCommandAsLocalUser "rm ${CURRENT_USERS_FILE}"
+}
+
+createLogFiles() {
+  saveLogFiles
+  runCommandAsLocalUser "echo 'input.${DATE}.log' > '${INPUT_FILE}'"
+  runCommandAsLocalUser "echo 'update.${DATE}.log' > '${UPDATE_LOG_FILE}'"
+  runCommandAsLocalUser "echo 'simple.${DATE}.log' > '${SIMPLE_LOG_FILE}'"
+  runCommandAsLocalUser "touch '${CURRENT_USERS_FILE}'"
+}
+
+updateUser() {
+  if [[ -n "${PUID}" ]]; then
+    usermod --non-unique --uid "${PUID}" ${USERNAME}
+  fi
+  if [[ -n "${PGID}" ]]; then
+    groupmod --non-unique --gid "${PGID}" ${USERGROUP}
+  fi
+  chown "${USERNAME}":"${USERGROUP}" -R "${INSTALL_DIRECTORY}"
+  chown "${USERNAME}":"${USERGROUP}" "${LOG_DIRECTORY}"
+}
+
+updateServer() {
+  cd "${INSTALL_DIRECTORY}"
+  if [[ "${AUTO_UPDATE}" == "true" ]]; then
+    chmod 777 -R /tmp
+    log "Updating Server"
+    runCommandAsLocalUser "steamcmd +runscript '${UPDATE_SCRIPT}' >> '${UPDATE_LOG_FILE}'"
+  fi
+}
+
+stopServer() {
+  local id=''
+  local waitTime=0;
+  local maximumWaitTime=30
+
+  echo "STOPPING" > "${PROCESS_STATUS_FILE}"
+
+  id="$(getServerProcessId)"
+  if [[ -n "${id}" ]]; then
+    log "Server Shutting Down"
+    stopProcess "${id}"
+    for (( waitTime=0; waitTime<=${maximumWaitTime}; waitTime++ )); do
+      if [[ -z "$(ps --pid ${id} --no-headers)" ]]; then
+        break
+      fi
+      sleep 1
+    done
+
+    if [[ -n "$(ps --pid ${id} --no-headers)" ]]; then
+      killProcess "${id}"
+      for (( waitTime=0; waitTime<=${maximumWaitTime}; waitTime++ )); do
+        if [[ -z "$(ps --pid ${id} --no-headers)" ]]; then
+          break
+        fi
+        sleep 1
+      done
+    fi
+
+    killProcess "$(getProcess 'tail' "${INPUT_FILE}")"
+    killProcess "$(getProcess 'tail' "${MAIN_LOG_FILE}")"
+    killProcess "$(getProcess "${START_SCRIPT}")"
+
+    tail --pid=${id} --follow=descriptor /dev/null
+  else
+    killProcess "$(getProcess 'steamcmd' "${UPDATE_SCRIPT}")"
+  fi
+}
+
+startServer() {
+  local id=''
+  local line=''
+
+  echo "STARTING" > "${PROCESS_STATUS_FILE}"
+
+  trap "{ echo 'Quit Signal Received' ; /build/stop.sh ; }" SIGQUIT
+  trap "{ echo 'Abort Signal Received' ; /build/stop.sh ; }" SIGABRT
+  trap "{ echo 'Interrupt Signal Received' ; /build/stop.sh ; }" SIGINT
+  trap "{ echo 'Terminate Signal Received' ; /build/stop.sh ; }" SIGTERM
+
+  updateUser
+  createLogFiles
+  if [[ "$(cat "${PROCESS_STATUS_FILE}")" == "STARTING" ]]; then
+    updateServer
+  fi
+
+  if [[ "$(cat "${PROCESS_STATUS_FILE}")" == "STARTING" ]]; then
+    log "Booting Server"
+    runCommandAsLocalUser "tail --follow=name --retry --lines=0 '${INPUT_FILE}' | '${START_SCRIPT}' ${START_ARGUMENTS}" &
+    while [[ "$(cat "${PROCESS_STATUS_FILE}")" == "STARTING" ]]; do
+      id="$(getServerProcessId)"
+      if [[ -n "${id}" ]]; then
+        break
+      fi
+      sleep 1
+    done
+    if [[ "$(cat "${PROCESS_STATUS_FILE}")" == "STARTING" && -n "${id}" ]]; then
+      echo "STARTED" > "${PROCESS_STATUS_FILE}"
+      sleep 10
+      while [[ "$(cat "${PROCESS_STATUS_FILE}")" == "STARTED" ]]; do
+        id="$(getServerProcessId)"
+        if [[ -z "${id}" ]]; then
+          break
+        fi
+        runCommandAsLocalUser "tail --pid=${id} --follow=name --lines +1 '${MAIN_LOG_FILE}' | perl /build/perl/logs.pl '${SIMPLE_LOG_FILE}' '${CURRENT_USERS_FILE}'"
+        sleep 1
+      done
+    else
+      stopServer
+    fi
+  fi
+  log "Server Shutdown"
+  echo "STOPPED" > "${PROCESS_STATUS_FILE}"
+
+  saveLogFiles
+
+  trap - SIGQUIT SIGABRT SIGINT SIGTERM
+}
